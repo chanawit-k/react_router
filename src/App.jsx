0caec142e1a29278026e864e6a09904f847760ac
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-02 11:29:00 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h2>home page</h2>,
+  },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h2>about page</h2>
+      </div>
+    ),
+  },
+])
+const App = () => {
+  return <RouterProvider router={router} />
+}
+export default App
