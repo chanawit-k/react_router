b7d0de0c84bafb62d46722014313584de3021dd8
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-02 20:40:52 +0700

    - add search Form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import { Form, useNavigation } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/SearchForm'
 const SearchForm = () => {
-  return <div>SearchForm</div>
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+  return (
+    <Wrapper>
+      <Form className="form">
+        <input
+          type="search"
+          name="search"
+          className="form-input"
+          defaultValue="vodka"
+        />
+        <button type="submit" className="btn" disabled={isSubmitting}>
+          {isSubmitting ? 'searching...' : 'search'}
+        </button>
+      </Form>
+    </Wrapper>
+  )
 }
 
 export default SearchForm
