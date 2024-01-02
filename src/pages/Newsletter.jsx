bb768e27068acce41c7673f692e355ad50270278
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-02 20:20:56 +0700

    - create form newsletter

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,7 +1,59 @@
 import React from 'react'
 
 const Newsletter = () => {
-  return <div>Newsletter</div>
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        our newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="john"
+        />
+      </div>
+      {/* last name */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="smith"
+        />
+      </div>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: '0.5rem' }}
+      >
+        submit
+      </button>
+    </form>
+  )
 }
 
 export default Newsletter
