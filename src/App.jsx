cc666f04c11bf483414b814a0bab424cc107a5f3
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-03 08:21:41 +0700

    - rm duplicate import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {
