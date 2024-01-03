91ce49ea66e956df75ff3d4185f6324a52cce705
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-03 08:24:25 +0700

    - implement useQuery in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from 'react-router-dom'
 import axios from 'axios'
 import SearchForm from '../components/SearchForm'
 import CocktailList from '../components/CocktailList'
+import { useQuery } from '@tanstack/react-query'
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
@@ -19,12 +20,12 @@ const searchCocktailsQuery = (searchTerm) => {
 export const loader = async ({ request }) => {
   const url = new URL(request.url)
   const searchTerm = url.searchParams.get('search') || ''
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
-  return { drinks: response.data.drinks, searchTerm }
+  return { searchTerm }
 }
 
 const Landing = () => {
-  const { searchTerm, drinks } = useLoaderData()
+  const { searchTerm } = useLoaderData()
+  const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm))
   return (
     <>
       <SearchForm searchTerm={searchTerm} />
