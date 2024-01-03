ec01c1b4202fe86408f6bb39da27d2355e6e4642
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-03 08:20:18 +0700

    - add search Param

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,10 +2,23 @@ import { useLoaderData } from 'react-router-dom'
 import axios from 'axios'
 import SearchForm from '../components/SearchForm'
 import CocktailList from '../components/CocktailList'
+
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-export const loader = async () => {
-  const searchTerm = 'milkShake'
+
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
+      return response.data.drinks
+    },
+  }
+}
+
+export const loader = async ({ request }) => {
+  const url = new URL(request.url)
+  const searchTerm = url.searchParams.get('search') || ''
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
   return { drinks: response.data.drinks, searchTerm }
 }
